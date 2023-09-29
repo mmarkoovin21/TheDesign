10a8421ca8be559d91d0f8ccb742a63f3bd06c43
Magdalena Markovinović <mmarkovin21@foi.hr>
2023-09-29 09:25:25 +0200

    puno tga

--- js/user.js
+++ js/user.js
@@ -8,35 +8,17 @@ document.addEventListener("click",function (e){
           myModal.show();
     }
   });
-  // Animacija skrolanja
-  // Funkcija koja će provjeriti kada se element nalazi na ekranu
-function isElementInViewport(element) {
-      const rect = element.getBoundingClientRect();
-      return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-  }
+  // Animacija naslova
   const prviNaslov = document.querySelector("#prvi");
   const drugiNaslov = document.querySelector("#drugi");
-  
-  // Funkcija koja će se pozvati pri svakom skrolanju
-  function handleScroll() {
-      if (isElementInViewport(prviNaslov)) {
-          prviNaslov.style.opacity = 1;
-          prviNaslov.style.animation = "fadeIn-prica 1.5s ease-in-out forwards";
-      }
-      if (isElementInViewport(drugiNaslov)) {
-          drugiNaslov.style.opacity = 1;
-          drugiNaslov.style.animation = "fadeIn-prica 1.5s ease-in-out forwards";
-      }
-  }
-  
-  // Dodajemo event listener za skrolanje
-  window.addEventListener("scroll", handleScroll);
-  
-  // Pokretanje animacije pri učitavanju stranice (ako su elementi odmah vidljivi)
-  handleScroll();
-  
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function (){
+        prviNaslov.style.opacity = 1;
+        prviNaslov.style.animation = "fadeIn-prica 1.5s ease-in-out forwards";
+    }, 1000);
+    setTimeout(function(){
+        drugiNaslov.style.opacity = 1;
+        drugiNaslov.style.animation = "fadeIn-prica 1.5s ease-in-out forwards";
+    }, 2200);
+});
