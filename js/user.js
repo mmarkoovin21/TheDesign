9fbfda8d7294c298627382aa0ac94635b4ec2527
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-02-05 13:06:30 +0100

    Pokušaj spajanja forme s gmailom.

--- js/user.js
+++ js/user.js
@@ -1,4 +1,3 @@
-
 // Za galeriju (modal)
 document.addEventListener("click",function (e){
     if(e.target.classList.contains("gallery-item")){
@@ -10,15 +9,10 @@ document.addEventListener("click",function (e){
 });
   // Animacija naslova
 const prviNaslov = document.querySelector("#prvi");
-const drugiNaslov = document.querySelector("#drugi");
 
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function (){
         prviNaslov.style.opacity = 1;
         prviNaslov.style.animation = "fadeIn-prica 1s ease-in-out forwards";
     }, 1000);
-    setTimeout(function(){
-        drugiNaslov.style.opacity = 1;
-        drugiNaslov.style.animation = "fadeIn-prica 1s ease-in-out forwards";
-    }, 2000);
 });
