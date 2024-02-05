54f3d49e1da12c3d6588d790c258786e7aa5080c
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-02-05 14:23:12 +0100

    Napravljena pomična galerija za desktop.

--- js/user.js
+++ js/user.js
@@ -1,12 +1,74 @@
 // Za galeriju (modal)
-document.addEventListener("click",function (e){
-    if(e.target.classList.contains("gallery-item")){
-          const src = e.target.getAttribute("src");
-          document.querySelector(".modal-img").src = src;
-          const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
-          myModal.show();
-    }
+
+//Polje slika
+let itemsImgs = document.querySelectorAll(".gallery-item");
+itemsImgsArr = Array.from(itemsImgs);
+
+//pop-up
+let popSlider = document.querySelector("section .modal-layer");
+let bgSlide = document.querySelector(".modal-layer .slide-img");
+
+let closeBtn = document.querySelector("#closeBtn");
+closeBtn.style.cssText = `cursor : pointer;`;
+// lijevo
+let prevBtn = document.querySelector("#prev");
+prevBtn.style.cssText = `cursor : pointer;`;
+// desno
+let nextBtn = document.querySelector("#next");
+nextBtn.style.cssText = `cursor : pointer;`;
+
+
+//index slike
+let activeIndex;
+
+for (let i = 0; i < itemsImgs.length; i++) {
+  itemsImgs[i].addEventListener("click", function (e) {
+    popSlider.style.display = "flex";
+    let activeSrc = e.target.src;
+    bgSlide.style.backgroundImage = `url(${activeSrc})`;
+    activeIndex = itemsImgsArr.indexOf(e.target);
+  });
+}
+
+closeBtn.addEventListener("click", removeSlider);
+
+function removeSlider() {
+  popSlider.style.display = "none";
+}
+
+nextBtn.addEventListener("click", nextSlider);
+
+function nextSlider() {
+  activeIndex++;
+  if (activeIndex == itemsImgs.length) {
+    activeIndex = 0;
+  }
+  let imgSrc = itemsImgsArr[activeIndex].src;
+  bgSlide.style.backgroundImage = `url(${imgSrc})`;
+}
+
+prevBtn.addEventListener("click", prevSlider);
+
+function prevSlider() {
+  activeIndex--;
+  if (activeIndex < 0) {
+    activeIndex = itemsImgs.length - 1;
+  }
+  let imgSrc = itemsImgsArr[activeIndex].src;
+  bgSlide.style.backgroundImage = `url(${imgSrc})`;
+}
+
+// keyboard events
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    removeSlider();
+  } else if (e.key == "ArrowRight") {
+    nextSlider();
+  } else if (e.key == "ArrowLeft") {
+    prevSlider();
+  }
 });
+
   // Animacija naslova
 const prviNaslov = document.querySelector("#prvi");
 
