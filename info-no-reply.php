9e65f18ef0a6616aadc3b1c1b8bcccdf38703923
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-03-06 18:19:10 +0100

    Finalna verzija izgleda povratnog emaila.

--- info-no-reply.php
+++ info-no-reply.php
@@ -2,32 +2,17 @@
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
+$template = "./mail-template.php";
+
 if (isset($_POST['primatelj-mail'])) {
     $to = $_POST['primatelj-mail'];
     $subject = 'Poruka zaprimljena';
-    $defaultPath = "https://thedesign.hr/dokumentacija/pic/new-logo-black.png";
-    $imageData = file_get_contents($imagePath);
-    $cid = md5(uniqid(time()));
-    $message = "
-            <html>
-            <head>
-            <title>Poštovani/a</title>
-            </head>
-            <body>
-                <p style=\"line-height: 0.5;\">Poštovani/a,</p><br>
-                <p style=\"line-height: 0.5;\">Zahvaljujemo na ukazanom interesu.</p>
-                <p style=\"line-height: 0.5;\"> Javit ćemo se u najkraćem mogućem roku.</p><br>
-                <p style=\"line-height: 0.5;\">Molimo da na ovaj mail odgovorite sa privitcima koje smatrate korisnima kako bi što kvalitetnije obradili Vaš upit.</p><br>
-                <p style=\"line-height: 0.5;\">Srdačno,</p><br>
-                <p style=\"line-height: 0.5; text-decoration: bold;\">The Design</p>
-                <p style=\"line-height: 0.5;\">E: <a href='mailto:info@thedesign.hr'>info@thedesign.hr</a></p>
-                <p style=\"line-height: 1;\">M: <a href='tel:0993452550'>099 / 345 - 2550</a></p>
-                <p style=\"line-height: 0.5;\">FB: <a href='https://www.facebook.com/The.Design.3D'>https://www.facebook.com/The.Design.3D</a></p>
-                <p style=\"line-height: 0.5;\">IG: <a href='https://www.instagram.com/the.design.3d/'>https://www.instagram.com/the.design.3d/</a></p><br>
-                <img style=\"width: 100px; height: 100px;\" src=\"cid:$cid\">
-            </body>
-            </html>
-            ";
+
+    if(file_exists($template)){
+        $message = file_get_contents($template);
+    }else{
+        die ("Nije moguće pronaći datoteku!");
+    }
 
     $headers = "From: info@thedesign.hr\r\n";
     $headers .= "Content-type: text/html;charset=utf-8\r\n";
---
+++ mail-template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Povratna poruka</title>
+</head>
+<body>
+                <p style="line-height: 0.5;">Poštovani/a,</p><br>
+                <p style="line-height: 0.5;">Zahvaljujemo na ukazanom interesu.</p>
+                <p style="line-height: 0.5;"> Javit ćemo se u najkraćem mogućem roku.</p><br>
+                <p style="line-height: 0.5;">Molimo da na ovaj mail odgovorite sa privitcima koje smatrate korisnima kako bi što kvalitetnije obradili Vaš upit.</p><br>
+                <p style="line-height: 0.5;">Srdačno,</p><br>
+                <p style="line-height: 0.5; text-decoration: bold;">The Design</p>
+                <p style="line-height: 0.5;">E: <a href='mailto:info@thedesign.hr'>info@thedesign.hr</a></p>
+                <p style="line-height: 0.5; padding-bottom: 10px;">M: <a href='tel:0993452550'>099 / 345 - 2550</a></p>
+                <p style="line-height: 0.5;">FB: <a href='https://www.facebook.com/The.Design.3D'>https://www.facebook.com/The.Design.3D</a></p>
+                <p style="line-height: 0.5;">IG: <a href='https://www.instagram.com/the.design.3d/'>https://www.instagram.com/the.design.3d/</a></p><br>
+                <img style="width: 100px; height: 100px;" src="https://thedesign.hr/dokumentacija/pic/new-logo-black.png">
+            </body>
+</html>
