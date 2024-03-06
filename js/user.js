dfcf855472ca1f1c911c2598a4be86b7b7c57ca3
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-03-06 19:38:00 +0100

    Završen dizajn početnog naslova.

--- js/user.js
+++ js/user.js
@@ -1,16 +1,11 @@
   // Animacija naslova
-const prviNaslov = document.getElementById("prvi");
-const drugiNaslov = document.getElementById("drugi");
+const prviNaslov = document.getElementById("naslov")
 
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function (){
         prviNaslov.style.opacity = 1;
         prviNaslov.style.animation = "fadeIn-prica 1s ease-in-out forwards";
     }, 1000);
-    setTimeout(function (){
-      drugiNaslov.style.opacity = 1;
-      drugiNaslov.style.animation = "fadeIn-prica 1s ease-in-out forwards";
-  }, 1000);
 });
 
 // Za galeriju (modal)
