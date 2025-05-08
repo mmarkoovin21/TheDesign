8f7ec818e8a1053d6401ed6e4753780fb136562d
Magdalena <mmarkovin21@foi.hr>
2025-05-08 15:49:24 +0200

    Modal sad radi ispravno

--- js/dohvacanjeSlika.js
+++ js/dohvacanjeSlika.js
@@ -3,20 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   dohvatiSlike(`${baseUrl}/apiIzrada.php`, 
                'izradaGalleryConteiner', 
-               'pic/izrada');
+               'pic/izrada').then(initGalleryModal);
 
   dohvatiSlike(`${baseUrl}/apiObnova.php`, 
                'obnovaGalleryConteiner', 
-               'pic/obnova');
+               'pic/obnova').then(initGalleryModal);;
 
   dohvatiPrveDvije(`${baseUrl}/apiIzrada.php`, 
                   'prveDvijeIzrada', 
-                  'pic/izrada');
+                  'pic/izrada').then(initGalleryModal);;
 
   dohvatiPrveDvije(`${baseUrl}/apiObnova.php`, 
                   'prveDvijeObnova', 
                   'pic/obnova', 
-                  '.galleryButtonContainer');
+                  '.galleryButtonContainer').then(initGalleryModal);;
 });
 
 async function dohvatiSlike(apiUrl, containerId, picFolder) {
@@ -72,3 +72,47 @@ async function dohvatiPrveDvije(apiUrl, containerId, picFolder, buttonSelector =
     console.error(e);
   }
 }
+
+function initGalleryModal() {
+    const items = document.querySelectorAll('.gallery-item');
+    const itemsArr = Array.from(items);
+  
+    const popSlider = document.querySelector('.modal-layer');
+    const bgSlide   = popSlider.querySelector('.slide-img');
+    const closeBtn  = popSlider.querySelector('#closeBtn');
+    const nextBtn   = popSlider.querySelector('#next');
+    const prevBtn   = popSlider.querySelector('#prev');
+  
+    let activeIndex = 0;
+  
+    document.body.addEventListener('click', e => {
+      if (e.target.matches('.gallery-item')) {
+        activeIndex = itemsArr.indexOf(e.target);
+        bgSlide.style.backgroundImage = `url(${e.target.src})`;
+        popSlider.style.display = 'flex';
+      }
+    });
+  
+    closeBtn.addEventListener('click', () => popSlider.style.display = 'none');
+    popSlider.addEventListener('click', e => {
+      if (e.target === popSlider) popSlider.style.display = 'none';
+    });
+  
+    nextBtn.addEventListener('click', () => {
+      activeIndex = (activeIndex + 1) % itemsArr.length;
+      bgSlide.style.backgroundImage = `url(${itemsArr[activeIndex].src})`;
+    });
+  
+    prevBtn.addEventListener('click', () => {
+      activeIndex = (activeIndex - 1 + itemsArr.length) % itemsArr.length;
+      bgSlide.style.backgroundImage = `url(${itemsArr[activeIndex].src})`;
+    });
+  
+    document.addEventListener('keydown', e => {
+      if (popSlider.style.display === 'flex') {
+        if (e.key === 'Escape') popSlider.style.display = 'none';
+        if (e.key === 'ArrowRight') nextBtn.click();
+        if (e.key === 'ArrowLeft')  prevBtn.click();
+      }
+    });
+  }
