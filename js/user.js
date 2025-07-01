856bc6f4149e2009978cb77d36b421abffc38c1e
Magdalena <mmarkovin21@foi.hr>
2025-07-01 22:05:11 +0200

    Dodan preload u head, asyc na JS itd.

--- js/user.js
+++ js/user.js
@@ -53,25 +53,3 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch(console.error);
 }
 );
-
-// funkcija za vraćanje navigacije
-/* var navbarLinksDiv = document.getElementById("navbarSupportedContent");
- var navButton = document.getElementsByClassName("navbar-toggler");
- navbarLinksDiv.addEventListener('click', () => {
-   navbarLinksDiv.classList.remove("show");
-   navButton.classList.add("collapsed");
- });
-
- // linkovi gumbi za galeriju
- document.querySelectorAll('.gallery-button').forEach(button => {  
-   button.addEventListener('click', function () {
-     window.location.href = this.getAttribute('data-href');
-   });
- });
-
- // linkovi gumbi za kontakt
- document.querySelectorAll('.upit').forEach(button => {
-   button.addEventListener('click', function () {
-     window.location.href = this.getAttribute('data-href');
-   });
- });*/
